7d892b1dec3a0d62bc54cf241dfd1d6c2f5a2976
romkravets <romann.kravets@gmail.com>
2019-05-14 00:08:40 +0300

    Added button loop

--- src/index/scripts/infoBox.js
+++ src/index/scripts/infoBox.js
@@ -1,5 +1,10 @@
 import { HTTPServece } from "../../common/scripts/http-service";
 
+const ROOT_CLASS_NAMES = 'slider';
+const SLIDES_CLASS_ALL = `.${ROOT_CLASS_NAMES}__image`;
+const BUTTON_PREV_CLASS = `.${ROOT_CLASS_NAMES}__prev`;
+const BUTTON_NEXT_CLASS = `.${ROOT_CLASS_NAMES}__next`;
+const VISIBLE_CLASS = 'visible';
 const URL = 'https://api.jsonbin.io/b/5c6de2a5ece18042ed2b1487';
 
 export class InfoBox {
@@ -8,6 +13,8 @@ export class InfoBox {
       this.infoBoxData = infoBoxData;
       this.httpService = new HTTPServece();
       this.slider;
+      this.buttonPrev;
+      this.buttonNext;
       this.render();
       //this.getList();
 
@@ -18,9 +25,10 @@ export class InfoBox {
    // }
 
    render() {
-      this.slider = document.createElement('div');
+      this.slider = document.createElement("div");
+      this.slider.classList.add("slider__images-container")
       this.rootElement.appendChild(this.slider);
-      
+
       for (let i = 0; i < this.infoBoxData.length; i++) {
          const { img, title, description } = this.infoBoxData[i];
          const infoBoxElement = `
@@ -39,16 +47,39 @@ export class InfoBox {
            </div>
          `;
          this.slider.innerHTML += infoBoxElement;
-
       }
 
-      const slides = document.querySelectorAll('.slider__image');
+      const buttonContainer = `
+         <div class="slider__btns">
+            <input class="slider__prev" type="button" value="PREV"></input>
+            <input class="slider__next" type="button" value="NEXT;"></input>
+         </div>
+      `;
+      this.rootElement.innerHTML += buttonContainer;
+
+      this.buttonPrev = document.querySelector(BUTTON_PREV_CLASS);
+      this.buttonPrev.addEventListener('click', () => {
+         slides[currentSlide].classList.remove(VISIBLE_CLASS);
+         currentSlide--;
+         if(currentSlide < 0 )
+         currentSlide = slides.length - 1;
+         slides[currentSlide].classList.add(VISIBLE_CLASS);
+      })
+
+      this.buttonNext = document.querySelector(BUTTON_NEXT_CLASS);
+      this.buttonNext.addEventListener('click', () => {
+         slides[currentSlide].classList.remove(VISIBLE_CLASS);
+         currentSlide = (currentSlide + 1) % slides.length;
+         slides[currentSlide].classList.add(VISIBLE_CLASS);
+      })
+
+      const slides = document.querySelectorAll(SLIDES_CLASS_ALL);
       let currentSlide = 0;
       const slideInterval = setInterval(() => {
-         slides[currentSlide].classList.remove('visible');
+         slides[currentSlide].classList.remove(VISIBLE_CLASS);
          currentSlide = (currentSlide + 1) % slides.length;
-         slides[currentSlide].classList.add('visible');
-      }, 3000);
+         slides[currentSlide].classList.add(VISIBLE_CLASS);
+      }, 7000);
    }
 }
 
